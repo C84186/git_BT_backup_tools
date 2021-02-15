d070347d0c4e9e0b25bd2a36a81d1041f6900dab
Smitten <77771103+C84186@users.noreply.github.com>
2021-02-15 17:12:36 +1100

    add setuptools integration

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pretty_print_bencoded',
+    version = '0.0.0.9001',
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = [
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        bencode2yaml=pretty_print:pretty_print 
+    ''',
+)

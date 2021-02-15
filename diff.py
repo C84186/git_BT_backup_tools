7151dd7e16f0271acf52fd9967ed53c0c2ad6aa1
Smitten <77771103+C84186@users.noreply.github.com>
2021-02-15 17:05:05 +1100

    basic implementation of bencode pretty printer

--- diff.py
+++ diff.py
@@ -27,13 +27,30 @@ def parse_bencoded(bencoded_path: pathlike_hint) -> typing.Optional[dict]:
     decoded = convert(decoded)
     return decoded
 
-def print_bencoded(bencoded: typing.Optional[dict]):
+def print_bencoded(bencoded: typing.Optional[dict], truncate_fields: bool):
     if not bencoded:
         print("---")
         print("---")
         return
 
+    if truncate_fields:
+        print("---")
+        print(yaml.dump({'apology': 'truncate_fields not implemented yet'}))
+        print("---")
+
     print("---")
     print(yaml.dump(bencoded))
     print("---")
 
+@click.command()
+@click.argument('bencoded_path')
+@click.option('--truncate_fields', default=True)
+def pretty_print_bencoded(bencoded_path: pathlike_hint, truncate_fields):
+    bencoded = parse_bencoded(bencoded_path)
+
+    print_bencoded(bencoded, truncate_fields)
+
+    
+
+if __name__ == '__main__':
+    

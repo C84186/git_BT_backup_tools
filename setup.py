bc8d646c598feef7a1c3afd9d2690176ea67e467
Smitten <77771103+C84186@users.noreply.github.com>
2021-02-15 18:40:24 +1100

    for now, remove setup

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
 
-setup(
-    name = 'pretty_print_bencoded',
-    version = '0.0.0.9001',
-    packages = find_packages(),
-    include_package_data = True,
-    install_requires = [
-        'Click',
-    ],
-    entry_points='''
-        [console_scripts]
-        bencode2yaml=pretty_print:pretty_print_bencoded 
-    ''',
-)
+setup()
+#  setup(
+    #  name = 'git_BT_backup_tools',
+    #  version = '0.0.0.9001',
+    #  packages = find_packages(),
+    #  include_package_data = True,
+    #  install_requires = [
+        #  'Click',
+    #  ],
+    #  entry_points='''
+        #  [console_scripts]
+        #  bencode2yaml=pretty_print:pretty_print_bencoded
+    #  ''',
+#  )

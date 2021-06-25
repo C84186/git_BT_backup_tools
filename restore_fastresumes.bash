7931b505402e6a0fd5097500c101397a7d7dc79c
Smitten <77771103+C84186@users.noreply.github.com>
2021-06-25 14:34:04 +1000

    check folder exists before running in restore script

--- restore_fastresumes.bash
+++ restore_fastresumes.bash
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 BT_BACKUP_DIR="${1:-$HOME/.local/share/data/qBittorrent/BT_backup}"
 
+if [ ! -d "$BT_BACKUP_DIR" ]; then
+  echo "$BT_BACKUP_DIR not found, exiting"
+  exit 1
+fi
+
 VALID_FILE=$(find "$BT_BACKUP_DIR" -name '*.fastresume' -size +1c -print -quit)
 find "$BT_BACKUP_DIR" -name '*.fastresume' -size 0 -exec cp "${VALID_FILE}" "{}" \;
 
